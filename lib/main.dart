118ef3109d430fd0b46330c20ae7fc8d74fa01c7
uhlapru57 <uhlaprumarma57@gmail.com>
2023-09-17 16:12:59 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: Scaffold(
+      appBar: AppBar(
+        title: const Text('APP BAR'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: (){},
+          )
+        ],
+        backgroundColor: Colors.blue,
+        leading: IconButton(
+          onPressed: (){},
+          icon: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: (){},
+          ),
+        ),
+      ),
+      body: Center(child: 
+      Container(
+        width: 500,
+        height: 500,
+        child: Image.asset('assets/images/img2.jpg')),
+      )
+    ),
+     
+    );
+  }
+}
